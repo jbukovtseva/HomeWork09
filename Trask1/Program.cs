d9ce1db5b1a542bd4ae4df3ccb47ad5ee717c5d6
Жанна Буковцева <jbukovtseva@mail.ru>
2022-09-03 15:59:58 +0300

    Trask1

---
+++ Trask1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите натуральное число N,\nчтобы вывести все натуральные числа от N до 1: ");
+string n = Console.ReadLine();
+
+var count = ExceptionHandling(n);
+
+if (count.exception && count.number >= 0)
+{
+    Numbers(count.number);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"{n} -> Некорректный ввод!");
+}
+
+void Numbers(int last)
+{
+    if (last == 0)
+    {
+        return;
+    }
+    Console.Write($"{last} ");
+    Numbers(last - 1);
+}
+
+(bool exception, int number) ExceptionHandling(string number)
+{
+    bool yesInt = int.TryParse(number, out int d);
+    if (!yesInt)
+    {
+        return (false, d);
+    }
+    return (true, d);
+}
